ce93815dabc2f7560b8c8b7be8f2b2da57f824d7
acslocum <acslocum@thoughtworks.com>
2014-10-06 17:09:37 -0400

    support for adding the X-FRAME-OPTIONS header to http responses to partially defeat click-jacking

--- src/com/slocumbrau/filter/InjectionAttackFilter.java
+++ src/com/slocumbrau/filter/InjectionAttackFilter.java
@@ -9,12 +9,17 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class InjectionAttackFilter implements Filter {
-  public static final String FILTER_XSS_PARAM_NAME = "filter_xss";
+  private static final String X_FRAME_VALUE = "SAMEORIGIN";
+private static final String X_FRAME_HEADER = "X-FRAME-OPTIONS";
+public static final String FILTER_XSS_PARAM_NAME = "filter_xss";
   public static final String FILTER_SQL_INJECTION_PARAM_NAME = "filter_sql_injection";
+  public static final String CLICK_JACKING_HEADER = "click_jacking_header";
   boolean filterXSS = true;
   boolean filterSQL = true;
+  boolean clickJacking = true;
   
   @Override
   public void destroy() {
@@ -23,15 +28,29 @@ public class InjectionAttackFilter implements Filter {
   @Override
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
     InjectionAttackWrapper wrapper = new InjectionAttackWrapper((HttpServletRequest) servletRequest, filterXSS, filterSQL);
+    filterClickJack(servletResponse);
     filterChain.doFilter(wrapper, servletResponse);
   }
 
-  @Override
+  private void filterClickJack(ServletResponse servletResponse) {
+    if(clickJacking) {
+        if(servletResponse instanceof HttpServletResponse) {
+        HttpServletResponse httpServletResponse = (HttpServletResponse)servletResponse;
+            if(!httpServletResponse.containsHeader(X_FRAME_HEADER)) {
+                httpServletResponse.addHeader(X_FRAME_HEADER, X_FRAME_VALUE);
+            }
+        }
+    }
+  }
+
+@Override
   public void init(FilterConfig config) throws ServletException {
     String filterXSSParam = config.getInitParameter(FILTER_XSS_PARAM_NAME);
     String filterSQLParam = config.getInitParameter(FILTER_SQL_INJECTION_PARAM_NAME);
+    String clickJackingParam = config.getInitParameter(CLICK_JACKING_HEADER);
     filterXSS = new Boolean(filterXSSParam);
     filterSQL = new Boolean(filterSQLParam);
+    clickJacking = new Boolean(clickJackingParam);
   }
 
 }
--- test/com/slocumbrau/filter/InjectionAttackFilterTest.java
+++ test/com/slocumbrau/filter/InjectionAttackFilterTest.java
@@ -1,12 +1,15 @@
 package com.slocumbrau.filter;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
+import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -45,6 +48,42 @@ public class InjectionAttackFilterTest {
 	InjectionAttackFilter filter = new InjectionAttackFilter();
 	filter.init(filterConfig);
 	assertFalse(filter.filterSQL);
+  }
+  
+  @Test
+  public void shouldAddXFrameOptionsHeaderWhenNotPresent() throws Exception {
+      FilterConfig filterConfig = Mockito.mock(FilterConfig.class);
+      Mockito.when(filterConfig.getInitParameter(InjectionAttackFilter.CLICK_JACKING_HEADER)).thenReturn("true");
+      InjectionAttackFilter filter = new InjectionAttackFilter();
+      filter.init(filterConfig);
+      HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+      Mockito.when(response.containsHeader("X-FRAME-OPTIONS")).thenReturn(false);
+      filter.doFilter(Mockito.mock(HttpServletRequest.class), response, Mockito.mock(FilterChain.class));
+      Mockito.verify(response).addHeader("X-FRAME-OPTIONS", "SAMEORIGIN");
+  }
+
+  @Test
+  public void shouldNotAddXFrameOptionsHeaderWhenAlreadyPresent() throws Exception {
+      FilterConfig filterConfig = Mockito.mock(FilterConfig.class);
+      Mockito.when(filterConfig.getInitParameter(InjectionAttackFilter.CLICK_JACKING_HEADER)).thenReturn("true");
+      InjectionAttackFilter filter = new InjectionAttackFilter();
+      filter.init(filterConfig);
+      HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+      Mockito.when(response.containsHeader("X-FRAME-OPTIONS")).thenReturn(true);
+      filter.doFilter(Mockito.mock(HttpServletRequest.class), response, Mockito.mock(FilterChain.class));
+      Mockito.verify(response, Mockito.never()).addHeader("X-FRAME-OPTIONS", "SAMEORIGIN");
+  }
+
+  @Test
+  public void shouldNotAddXFrameOptionsHeaderWhenDisabled() throws Exception {
+      FilterConfig filterConfig = Mockito.mock(FilterConfig.class);
+      Mockito.when(filterConfig.getInitParameter(InjectionAttackFilter.CLICK_JACKING_HEADER)).thenReturn("false");
+      InjectionAttackFilter filter = new InjectionAttackFilter();
+      filter.init(filterConfig);
+      HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+      Mockito.when(response.containsHeader("X-FRAME-OPTIONS")).thenReturn(false);
+      filter.doFilter(Mockito.mock(HttpServletRequest.class), response, Mockito.mock(FilterChain.class));
+      Mockito.verify(response, Mockito.never()).addHeader("X-FRAME-OPTIONS", "SAMEORIGIN");
   }
 
   @Test
